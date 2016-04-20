138220ae92ca1966b351697c7d0d4fec6cca9d7e
Lewis Kabui <lewis.maina@andela.com>
2016-04-20 18:32:00 +0300

    Add models  Create the Goods and Services models

--- models.py
+++ models.py
@@ -12,8 +12,32 @@ class Outlets(db.Model):
 	name = db.Column(db.String(100))
 	postal_address = db.Column(db.String(100))
 
+	def __init__(self, name, postal_address):
+		self.name = name
+		self.postal_address = postal_address
+
 	def __repr__(self):
-		return '<name {0}'.format(self.name)
+		return '<Outlets {0}>'.format(self.name)
+
+
+class Goods(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100))
+	price = db.Column(db.Integer)
+	necessary = db.Column(db.Boolean)
+
+	def __repr__(self):
+		return '<Goods {0}'.format(self.name)
+
+
+class Services(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100))
+	price = db.Column(db.Integer)
+
+	def __repr__(self):
+		return '<Services {0}'.format(self.name)
+
 
 if __name__ == '__main__':
 	ans = input('\n\nProceed to create database? [y|n] \
