bd46980b287aa34a2c242574a61c9f72d1d1506d
Lewis Kabui <lewis.maina@andela.com>
2016-04-21 11:18:06 +0300

    Create POST logic for outlet endpoint
    Use flask-restful package to parse 'name' and 'postal_address' arguments
    from the client
    Create object using Outlets model
    Persist object
    Use marshmallow package to format newly created object to JSON format
    for display

--- starters.py
+++ starters.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import request
+from flask_restful import reqparse
 
-app = Flask(__name__)
+from models import Outlets
+from serializer import OutletSchema
+from app import app, db
 
 
 @app.route('/')
@@ -8,12 +11,32 @@ def hello():
 	return 'Welcome home!'
 
 
-@app.route('/outlets', methods=['GET', 'POST', 'PUT'])
+@app.route('/outlets/', methods=['GET', 'POST'])
 def outlets():
 	if request.method == 'GET':
 		return 'Your outlets!'
 	if request.method == 'POST':
-		return 'Write some posting logic here'
+		# specify data fields to look out for from user
+		parser = reqparse.RequestParser()
+		parser.add_argument('name', required=True)
+		parser.add_argument('postal_address')
+		values = parser.parse_args()
+		# create outlet object from data
+		new_outlet = Outlets(**values)
+		db.session.add(new_outlet)
+		db.session.commit()
+		# display details of object just created
+		outlet_schema = OutletSchema()
+		json_result = outlet_schema.dumps(new_outlet)
+		return json_result.data, 201
+
+
+@app.route('/outlets/<outlet_id>/', methods=['GET', 'PUT'])
+def outlets_detail(outlet_id):
+	if request.method == 'GET':
+		return 'Outlet of id: {0}!'.format(outlet_id)
+	if request.method == 'PUT':
+		return 'Detail view for outlets'
 
 
 @app.route('/expenses')
@@ -26,4 +49,4 @@ def accounts():
 	return 'Your accounts!'
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
