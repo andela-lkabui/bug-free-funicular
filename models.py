4945515b3c5a94fe63c97ab373536fc3e7ecdc7d
Lewis Kabui <lewis.maina@andela.com>
2016-04-21 11:16:31 +0300

    Import the db object from newly created app file

--- models.py
+++ models.py
@@ -1,10 +1,4 @@
-import os
-
-from starters import app
-from flask_sqlalchemy import SQLAlchemy
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+from app import db
 
 
 class Outlets(db.Model):
