0b24b249f0d72c0489da56ad7b2e68f12c518eb0
Lewis Kabui <lewis.maina@andela.com>
2016-04-23 15:12:55 +0300

    Create schema for the Goods model
    Create a marshmallow schema for the goods model to facilitate conversion
    between JSON encoded string and model objects
    Add the 'id' field to the OutletSchema for display
    Create a customized method annotated with post_load to return a Goods
    object when <schema>.load(dict) is called

--- serializer.py
+++ serializer.py
@@ -1,6 +1,20 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
+
+from models import Goods
 
 
 class OutletSchema(Schema):
+	name = fields.Integer()
 	name = fields.Str()
 	postal_address = fields.Str()
+
+
+class GoodsSchema(Schema):
+	id = fields.Integer()
+	name = fields.Str()
+	price = fields.Str()
+	necessary = fields.Boolean()
+
+	@post_load
+	def create_good(self, data):
+		return Goods(**data)
