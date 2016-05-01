5fa339a9b60adf53fba6e9811bd0f8d51a97a945
Lewis Kabui <lewis.maina@andela.com>
2016-05-01 12:18:06 +0300

    Create User model
    Create a user model with id, username, password_hash and is_active fields
    Define method hash_password to hash plain text password for storage in the
    database
    Define method verify_password to compare plain text passwords from the client
    against a hash from the database to verify authentication credentials
    Define method generate_auth_token to generate an authentication token for use
    by the client
    Define method verify_auth_token which receives a token and returns a truthy value
    if the token is valid and a falsy value if the token is expired or bad

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
+from flask import current_app
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+	BadSignature, SignatureExpired)
+
 from app import db
 
 
@@ -21,7 +26,7 @@ class Goods(db.Model):
 	necessary = db.Column(db.Boolean)
 
 	def __repr__(self):
-		return '<Goods {0}'.format(self.name)
+		return '<Goods {0}>'.format(self.name)
 
 
 class Services(db.Model):
@@ -30,8 +35,40 @@ class Services(db.Model):
 	price = db.Column(db.Integer)
 
 	def __repr__(self):
-		return '<Services {0}'.format(self.name)
+		return '<Services {0}>'.format(self.name)
 
+
+class User(db.Model):
+	user_id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(100))
+	password_hash = db.Column(db.String(200))
+	is_active = db.Column(db.Boolean, default=False)
+
+	def __repr__(self):
+		return '<User {0}>'.format(self.username)
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+
+	def generate_auth_token(self, expiration=600):
+		s = Serializer(current_app.config.get('SECRET_KEY'), expires_in=expiration)
+		return s.dumps({'id': self.id})
+
+	@staticmethod
+	def verify_auth_token(token):
+		s = Serializer(current_app.config.get('SECRET_KEY'))
+		try:
+			data = s.loads(token)
+		except SignatureExpired:
+			return None
+		except BadSignature:
+			return None
+
+		user = User.query.get(data.get('id'))
+		return user
 
 if __name__ == '__main__':
 	ans = input('\n\nProceed to create database? [y|n] \
