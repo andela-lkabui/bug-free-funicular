0f9fdacbace7e1fab125130fffb93c6307e046b3
Lewis Kabui <lewis.maina@andela.com>
2016-06-02 15:04:02 +0300

    (feature) Use fixtures in unittest
    Create a user fixture in the unittest to test for registration of a user
    when the username already exists

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,25 +2,29 @@ import os
 import unittest
 
 from faker import Factory
+from flask.ext.fixtures import FixturesMixin
 
-from app import db
+from models import db
 from starters import app
 
+test_url = os.environ.get('TEST_DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = test_url
+app.config['TESTING'] = True
 
-class TestBase(unittest.TestCase):
+FixturesMixin.init_app(app, db)
+
+
+class TestBase(unittest.TestCase, FixturesMixin):
     """Base unittest class to be inherited from by other test classes."""
 
     def setUp(self):
         """Method to initialize test resources for every test that is run."""
-        test_url = os.environ.get('TEST_DATABASE_URL')
-        app.config['SQLALCHEMY_DATABASE_URI'] = test_url
         db.create_all()
         self.client = app.test_client()
         self.fake = Factory.create()
+        # import ipdb; ipdb.set_trace()
 
     def tearDown(self):
         """Method to destroy/free up test resources once a test is run."""
         db.session.remove()
         db.drop_all()
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-        
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -4,7 +4,9 @@ from models import User
 
 class TestRegistration(TestBase):
     """Create tests for user registration routes."""
-
+    fixtures = ['user.json']
+    # import ipdb; ipdb.set_trace()
+    
     def test_successful_registration(self):
         """Test successful registration when valid username and password is
         provided."""
@@ -18,7 +20,7 @@ class TestRegistration(TestBase):
         self.assertEqual(response.status, '201 CREATED')
         self.assertEqual(response.status_code, 201)
         # Assert that the record is in the database
-        self.assertEqual(User.query.get(1).username, user.get('username'))
+        self.assertTrue(User.query.filter_by(username=username).first())
         self.assertTrue(b'User successfully registered' in response.data)
 
     def test_registration_without_password(self):
@@ -32,7 +34,7 @@ class TestRegistration(TestBase):
         self.assertEqual(response.status, '400 BAD REQUEST')
         self.assertEqual(response.status_code, 400)
         # Assert that user has not been created in the database
-        self.assertEqual(len(User.query.all()), 0)
+        self.assertEqual(len(User.query.filter_by(username=username).all()), 0)
         self.assertTrue(b'Password missing' in response.data)
 
     def test_registration_without_username(self):
@@ -46,7 +48,8 @@ class TestRegistration(TestBase):
         self.assertEqual(response.status, '400 BAD REQUEST')
         self.assertEqual(response.status_code, 400)
         # Assert that user has not been created in the database
-        self.assertEqual(len(User.query.all()), 0)
+        # Testing for length 1 to account for the fixture
+        self.assertEqual(len(User.query.all()), 1)
         self.assertTrue(b'Username missing' in response.data)
 
     def test_registration_invalid_credentials(self):
@@ -59,5 +62,21 @@ class TestRegistration(TestBase):
         self.assertEqual(response.status, '400 BAD REQUEST')
         self.assertEqual(response.status_code, 400)
         # Assert that user has not been created in the database
-        self.assertEqual(len(User.query.all()), 0)
+        # Testing for length 1 to account for the fixture
+        self.assertEqual(len(User.query.all()), 1)
         self.assertTrue(b'Username missing' in response.data)
+
+    def test_registration_existing_username(self):
+        # Assert that there is a fixture already
+        self.assertEqual(len(User.query.all()), 1)
+        username = User.query.first().username
+        password = self.fake.password()
+
+        user = {
+            'username': username,
+            'password': password
+        }
+        response = self.client.post('/auth/new/', data=user)
+        self.assertEqual(response.status, '400 BAD REQUEST')
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue(b'User already exists' in response.data)
