f0635196456e8243b04b1755373422e14572733f
Lewis Kabui <lewis.maina@andela.com>
2016-07-20 12:46:23 +0300

    (feature) Create an Accounts model serializer

--- serializer.py
+++ serializer.py
@@ -1,30 +1,41 @@
 from marshmallow import Schema, fields, post_load
 
-from models import Goods, Services
+from models import Goods, Services, Accounts
 
 
 class OutletSchema(Schema):
-	name = fields.Integer()
-	name = fields.Str()
-	postal_address = fields.Str()
+    name = fields.Integer()
+    name = fields.Str()
+    postal_address = fields.Str()
 
 
 class GoodsSchema(Schema):
-	id = fields.Integer()
-	name = fields.Str()
-	price = fields.Str()
-	necessary = fields.Boolean()
+    id = fields.Integer()
+    name = fields.Str()
+    price = fields.Str()
+    necessary = fields.Boolean()
 
-	@post_load
-	def create_good(self, data):
-		return Goods(**data)
+    @post_load
+    def create_good(self, data):
+        return Goods(**data)
 
 
 class ServicesSchema(Schema):
-	id = fields.Integer()
-	name = fields.Str()
-	price = fields.Str()
+    id = fields.Integer()
+    name = fields.Str()
+    price = fields.Str()
 
-	@post_load
-	def create_service(self, data):
-		return Services(**data)
+    @post_load
+    def create_service(self, data):
+        return Services(**data)
+
+
+class AccountsSchema(Schema):
+    name = fields.Str()
+    phone_no = fields.Str()
+    account_no = fields.Str()
+    account_provider = fields.Str()
+
+    @post_load
+    def create_service(self, data):
+        return Accounts(**data)
