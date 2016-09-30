d8519641ba334eeb47ba22ff7b02bba703efeaa5
Lewis Kabui <lewis.maina@andela.com>
2016-09-30 15:35:12 +0300

    (feature) Define tests for the put functionality of Goods resource

--- tests/test_goods.py
+++ tests/test_goods.py
@@ -326,3 +326,218 @@ class TestGoods(TestBase):
         self.assertEqual(response.status_code, 404)
         self.assertTrue('Good of id 4 does not exist' in response.data)
 
+    def test_put_goods_resource_successful(self):
+        """
+        Test a successful put resource on the Goods resource.
+        """
+        user = {
+            'username': 'pythonista',
+            'password': 'pythonista'
+        }
+        response = self.client.post('/auth/login/', data=user)
+        self.assertEqual(response.status, '200 OK')
+        json_data = json.loads(response.data)
+        token = json_data.get('token')
+        headers = {
+            'username': token
+        }
+        data = {
+            'name': self.fake.name(),
+            'necessary': self.fake.boolean(),
+            'price': self.fake.random_number()
+        }
+        response = self.client.put('/goods/1/', data=data, headers=headers)
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse('Silvio Wolf' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertEqual(edited_good.name, data.get('name'))
+
+    def test_put_goods_resource_not_owner(self):
+        """
+        Test a request where Good of good_id does not belong to authenticated
+        user.
+        """
+        user = {
+            'username': 'ruby',
+            'password': 'pythonista'
+        }
+        response = self.client.post('/auth/login/', data=user)
+        self.assertEqual(response.status, '200 OK')
+        json_data = json.loads(response.data)
+        token = json_data.get('token')
+        headers = {
+            'username': token
+        }
+        data = {
+            'name': self.fake.name(),
+            'necessary': self.fake.boolean(),
+            'price': self.fake.random_number()
+        }
+        response = self.client.put('/goods/1/', data=data, headers=headers)
+        self.assertEqual(response.status, '403 FORBIDDEN')
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue(
+            'Access to good is restricted to owner' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertNotEqual(edited_good.name, data.get('name'))
+
+    def test_put_goods_resource_non_existent_good_id(self):
+        """
+        Test a put request where Good of id good_id does not exist.
+        """
+        user = {
+            'username': 'pythonista',
+            'password': 'pythonista'
+        }
+        response = self.client.post('/auth/login/', data=user)
+        self.assertEqual(response.status, '200 OK')
+        json_data = json.loads(response.data)
+        token = json_data.get('token')
+        headers = {
+            'username': token
+        }
+        data = {
+            'name': self.fake.name(),
+            'necessary': self.fake.boolean(),
+            'price': self.fake.random_number()
+        }
+        response = self.client.put('/goods/24/', data=data, headers=headers)
+        self.assertEqual(response.status, '404 NOT FOUND')
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(
+            'Good of id 24 does not exist' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertNotEqual(edited_good.name, data.get('name'))
+
+    def test_put_goods_resource_no_authentication_token(self):
+        """
+        Test a put request where authentication token has not been provided.
+        """
+        data = {
+            'name': self.fake.name(),
+            'necessary': self.fake.boolean(),
+            'price': self.fake.random_number()
+        }
+        response = self.client.put('/goods/1/', data=data)
+        self.assertEqual(response.status, '401 UNAUTHORIZED')
+        self.assertEqual(response.status_code, 401)
+        self.assertTrue(
+            'Unauthenticated request' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertNotEqual(edited_good.name, data.get('name'))
+
+    def test_put_goods_resource_invalid_authentication_token(self):
+        """
+        Test a put request where invalid authentication token has been
+        provided.
+        """
+        token = self.fake.sha256()
+        headers = {
+            'username': token
+        }
+        data = {
+            'name': self.fake.name(),
+            'necessary': self.fake.boolean(),
+            'price': self.fake.random_number()
+        }
+        response = self.client.put('/goods/24/', data=data, headers=headers)
+        self.assertEqual(response.status, '403 FORBIDDEN')
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue(
+            'Invalid token' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertNotEqual(edited_good.name, data.get('name'))
+
+    def test_put_goods_resource_only_name_parameter_provided(self):
+        """
+        Test a successful put request on the Goods resource when name is the
+        only parameter provided.
+        """
+        user = {
+            'username': 'pythonista',
+            'password': 'pythonista'
+        }
+        response = self.client.post('/auth/login/', data=user)
+        self.assertEqual(response.status, '200 OK')
+        json_data = json.loads(response.data)
+        token = json_data.get('token')
+        headers = {
+            'username': token
+        }
+        data = {
+            'name': self.fake.name()
+        }
+        unedited_good = Goods.query.get(1)
+        necessary = unedited_good.necessary
+        price = unedited_good.price
+        response = self.client.put('/goods/1/', data=data, headers=headers)
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse('Silvio Wolf' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertEqual(edited_good.name, data.get('name'))
+        self.assertEqual(edited_good.necessary, necessary)
+        self.assertEqual(edited_good.price, price)
+
+    def test_put_goods_resource_only_necessary_parameter_provided(self):
+        """
+        Test a successful put request on the Goods resource when necessary is
+        the only parameter provided.
+        """
+        user = {
+            'username': 'pythonista',
+            'password': 'pythonista'
+        }
+        response = self.client.post('/auth/login/', data=user)
+        self.assertEqual(response.status, '200 OK')
+        json_data = json.loads(response.data)
+        token = json_data.get('token')
+        headers = {
+            'username': token
+        }
+        data = {
+            'necessary': self.fake.boolean()
+        }
+        unedited_good = Goods.query.get(1)
+        name = unedited_good.name
+        price = unedited_good.price
+        response = self.client.put('/goods/1/', data=data, headers=headers)
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Silvio Wolf' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertEqual(edited_good.necessary, data.get('necessary'))
+        self.assertEqual(edited_good.name, name)
+        self.assertEqual(edited_good.price, price)
+
+    def test_put_goods_resource_only_price_parameter_provided(self):
+        """
+        Test a successful put request on the Goods resource when price is
+        the only parameter provided.
+        """
+        user = {
+            'username': 'pythonista',
+            'password': 'pythonista'
+        }
+        response = self.client.post('/auth/login/', data=user)
+        self.assertEqual(response.status, '200 OK')
+        json_data = json.loads(response.data)
+        token = json_data.get('token')
+        headers = {
+            'username': token
+        }
+        data = {
+            'price': self.fake.random_number()
+        }
+        unedited_good = Goods.query.get(1)
+        name = unedited_good.name
+        necessary = unedited_good.necessary
+        response = self.client.put('/goods/1/', data=data, headers=headers)
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Silvio Wolf' in response.data)
+        edited_good = Goods.query.get(1)
+        self.assertEqual(edited_good.price, data.get('price'))
+        self.assertEqual(edited_good.name, name)
+        self.assertEqual(edited_good.necessary, necessary)
